ea58a8bb43d5edc0a3f3a2ce68dc3696c68ea6d0
Christian Gustavo <christiangomes00@gmail.com>
2020-10-09 08:13:52 -0300

    Criando método para novas transações

--- dev/test.js
+++ dev/test.js
@@ -2,8 +2,16 @@ const Blockchain = require('./blockchain');
 
 const bitcoin = new Blockchain();
 
-bitcoin.createNewBlock(2389, 'OINA90SSDNF90N', '90ASDAS90ASDASD');
-bitcoin.createNewBlock(111, 'IQOWUEIOPQWU', '0ZX9C0XZXZCZX0C');
-bitcoin.createNewBlock(2899, 'NMNBMBNERTRTMB', 'DFGTTTDFGTERT');
+bitcoin.createNewBlock(987987, 'AS0A9S9ASAN0A9N0A9', 'NA09NA9N0A90AN');
 
-console.log(bitcoin);
+bitcoin.createNewTransaction(100, 'ALEXD9AS89DSA', 'JENN1D23AS1ASD1');
+
+bitcoin.createNewBlock(123123, 'QE87QW9EQW7E9Q', 'V97N9VB87N9V7B');
+
+bitcoin.createNewTransaction(50, 'ALEXD9AS89DSA', 'JENN1D23AS1ASD1');
+bitcoin.createNewTransaction(300, 'ALEXD9AS89DSA', 'JENN1D23AS1ASD1');
+bitcoin.createNewTransaction(2000, 'ALEXD9AS89DSA', 'JENN1D23AS1ASD1');
+
+bitcoin.createNewBlock(9879879, 'ASDASDA8S97D', 'ASDASD321ASDAS1');
+
+console.log(bitcoin.chain[2]);
