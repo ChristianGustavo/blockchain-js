690b09e4fcc180add35bd4e3dad2b07a1f1f0092
Christian Gustavo <christiangomes00@gmail.com>
2020-10-10 18:05:32 -0300

    Criando endpoint para registrar nó e fazer broadcast para os demais nós

--- dev/networkNode.js
+++ dev/networkNode.js
@@ -4,6 +4,7 @@ const bodyParser = require('body-parser');
 const Blockchain = require('./blockchain');
 const uuid = require('uuid');
 const port = process.argv[2];
+const rp = require('request-promise');
 
 const nodeAddress = uuid.v1().split('-').join('');
 
@@ -41,6 +42,49 @@ app.get('/mine', function(req, res) {
     note: 'New block mined successfully',
     block: newBlock
   });
+});
+
+// Registra um nó e faz broadcast para a rede
+app.post('/register-and-broadcast-node', function (req, res) {
+  const newNodeUrl = req.body.newNodeUrl;
+  if (bitcoin.networkNodes.indexOf(newNodeUrl) == -1) bitcoin.networkNodes.push(newNodeUrl);
+
+  const regNodesPromises = [];
+  bitcoin.networkNodes.forEach(networkNodeUrl => {
+    const requestOptions = {
+      uri: networkNodeUrl + '/register-node',
+      method: 'POST',
+      body: { newNodeUrl: newNodeUrl },
+      json: true
+    };
+
+    regNodesPromises.push(rp(requestOptions));
+  });
+
+  Promise.all(regNodesPromises)
+    .then(data => {
+      const bulkRegisterOptions = {
+        uri: newNodeUrl + '/register-nodes-bulk',
+        method: 'POST',
+        body: { allNetworkNodes: [ ...bitcoin.networkNodes, bitcoin.currentNodeUrl ] },
+        json: true
+      };
+
+      return rp(bulkRegisterOptions);
+    })
+    .then(data => {
+      res.json({ note: 'New node registered with network successfully.' });
+    });
+});
+
+// Registra um nó na rede
+app.post('/register-node', function(req, res) {
+
+});
+
+// Registra vários nós de uma vez
+app.post('/register-nodes-bulk', function(req, res) {
+
 });
 
 app.listen(port, function() {
