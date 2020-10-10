0317b2c2a3bec4e7c5294a7928284ba10e63fe78
Christian Gustavo <christiangomes00@gmail.com>
2020-10-10 15:20:23 -0300

    Criando o bloco de origem (Genesis Block)

--- dev/test.js
+++ dev/test.js
@@ -1,23 +1,4 @@
 const Blockchain = require('./blockchain');
 const bitcoin = new Blockchain();
 
-const previousBlockHash = 'ASD89A7S89DA5SD65A4';
-const currentBlockData = [
-    {
-        amount: 10,
-        sender: 'ANSDNASNDASDAPOSEQ',
-        recipient: 'WQE87QW89E7QAS4D56A4S'
-    },
-    {
-        amount: 30,
-        sender: 'QWIOUEOIPQWU123IOU1P',
-        recipient: 'LSDJALÇK23ASLDKKAS12'
-    },
-    {
-        amount: 200,
-        sender: '8D7F8SAD5DSF65SDA',
-        recipient: '1F2SDA1FSADDS3F21ASD'
-    }
-];
-
-console.log(bitcoin.hashBlock(previousBlockHash, currentBlockData, 70298));
+console.log(bitcoin);
